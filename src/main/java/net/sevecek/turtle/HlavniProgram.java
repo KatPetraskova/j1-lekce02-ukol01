c79c14f621410e4364c1b8d8d52f9d31ce42ddd1
Kateřina Petrášková <ka.petraskova@gmail.com>
2022-04-14 14:39:12 +0200

    1. cast - metoda nakresliPrasatko

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -8,6 +8,45 @@ public class HlavniProgram {
         Turtle zofka;
 
         zofka = new Turtle();
+
+        nakresliPrasatko(zofka);
+    }
+
+    private void nakresliPrasatko(Turtle zofka) {
+        for (int i = 0; i < 4; i++) {
+            zofka.turnRight(90);
+            zofka.move(100);
+        }
+        zofka.turnLeft(135);
+        zofka.move(70.71);
+        zofka.turnLeft(90);
+        zofka.move(70.71);
+        zofka.move(30);
+        zofka.penUp();
+        zofka.turnRight(180);
+        zofka.move(30);
+        zofka.penDown();
+        zofka.turnLeft(90);
+        zofka.move(30);
+        zofka.penUp();
+        zofka.turnRight(180);
+        zofka.move(30);
+        zofka.turnRight(45);
+        zofka.move(100);
+        zofka.penDown();
+        zofka.turnRight(45);
+        zofka.move(30);
+        zofka.penUp();
+        zofka.turnRight(180);
+        zofka.move(30);
+        zofka.penDown();
+        zofka.turnLeft(90);
+        zofka.move(30);
+        zofka.penUp();
+        zofka.turnRight(180);
+        zofka.move(30);
+        zofka.turnLeft(45);
+        zofka.move(100);
     }
 
 }
