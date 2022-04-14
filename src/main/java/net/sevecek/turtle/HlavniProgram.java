eaf946e4494c2f25c798ee7ad7c46c2535418803
Kateřina Petrášková <ka.petraskova@gmail.com>
2022-04-14 15:16:24 +0200

    2. cast - metody pro nakresleni 8uhelniku, kolecka a slunicka + zofka kresli obrazce

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -7,11 +7,79 @@ public class HlavniProgram {
     public void main(String[] args) {
         Turtle zofka;
 
+        // instance zofky
         zofka = new Turtle();
 
-        nakresliPrasatko(zofka);
+        // zofka se posune vlevo, aby mela misto na kresleni :)
+        zofka.penUp();
+        zofka.turnLeft(90);
+        zofka.move(350);
+        zofka.turnRight(90);
+        zofka.penDown();
+        zofka.setPenWidth(5);
+
+        // zofka nakresli prasatko
+        //nakresliPrasatko(zofka);
+
+        // zofka nakresli 8uhelnik
+        nakresliOsmiuhelnik(zofka);
+
+        // zofka se posune vpravo
+        zofkaSePosuneVpravo(zofka);
+
+        // zofka nakreslni kolecko
+        nakresliKolecko(zofka);
+
+        // zofka se posune vpravo
+        zofkaSePosuneVpravo(zofka);
+
+        // zofka nakresli slunicko
+        nakresliSlunicko(zofka);
+
+
     }
 
+    // metoda pro posun zofky vpravo
+    private void zofkaSePosuneVpravo(Turtle zofka) {
+        zofka.penUp();
+        zofka.turnRight(90);
+        zofka.move(300);
+        zofka.turnLeft(90);
+        zofka.penDown();
+    }
+
+    // metoda pro nakresleni slunicka
+    private void nakresliSlunicko(Turtle zofka) {
+        for (int l = 0; l < 9; l++) {
+            zofka.turnLeft(90);
+            zofka.move(30);
+            zofka.turnLeft(180);
+            zofka.move(30);
+            zofka.turnLeft(70);
+            zofka.move(15);
+            zofka.turnRight(20);
+            zofka.move(15);
+        }
+    }
+
+
+    // metoda pro nakresleni kolecka
+    private void nakresliKolecko(Turtle zofka) {
+        for (int k = 0; k < 18; k++) {
+            zofka.turnRight(20);
+            zofka.move(20);
+        }
+    }
+
+    // metoda pro nakresleni osmiuhelniku
+    private void nakresliOsmiuhelnik(Turtle zofka) {
+        for (int j = 0; j < 8; j++) {
+            zofka.turnRight(45);
+            zofka.move(30);
+        }
+    }
+
+    // metoda pro nakresleni prasatka
     private void nakresliPrasatko(Turtle zofka) {
         for (int i = 0; i < 4; i++) {
             zofka.turnRight(90);
